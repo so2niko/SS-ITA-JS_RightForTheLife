5039c5087b187699c52aa4e65339ef7ecf260943
alexboklag <sanya32102@gmail.com>
2020-01-15 20:16:53 +0200

    Add footer component

---
+++ right-for-life_front/src/app/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../assets/logo.svg';
+
+const Footer = () => {
+  return <ul className="h-16 bg-gray-100 shadow-md flex justify-center items-center font-bold text-2xl text-gray-600 mt-4">
+  <li className="ml-32">0932350370</li>
+  <li className="mx-6"><img src={logo} alt='logo' /></li>
+  <li>olyaum76@gmail.com</li>
+</ul>;
+}
+
+export default Footer;
