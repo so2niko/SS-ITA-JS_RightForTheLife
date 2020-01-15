ec02a6ecb3983d3ed280ad18f060621cb500a03f
alexboklag <sanya32102@gmail.com>
2020-01-15 18:58:41 +0200

    Add animals list component

---
+++ right-for-life_front/src/app/components/AnimalsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AnimalCard from '../components/AnimalCard';
+
+const AnimalsList = ({animals}) => {
+  return (
+    <ul className="flex flex-wrap justify-center pt-20">
+      {
+        animals.map(animal => {
+          return <AnimalCard key={animal.id.toString()} animal={animal} />
+        })
+      }
+    </ul>
+  )
+}
+
+export default AnimalsList;
