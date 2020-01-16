f6399929d43f84916e9af97c03a4ea06129d7201
alexboklag <sanya32102@gmail.com>
2020-01-16 11:00:03 +0200

    Change component background

--- right-for-life_front/src/app/components/Header.js
+++ right-for-life_front/src/app/components/Header.js
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.svg';
 
 const Header = () => {
   return (
-    <ul className="fixed w-full h-16 bg-gray-100 shadow-md flex justify-center items-center font-bold text-2xl text-gray-600 z-50">
+    <ul className="fixed w-full h-16 bg-lightgray-100 shadow-md flex justify-center items-center font-bold text-2xl text-gray-600 z-50">
       <li className="mx-4 cursor-pointer "><img src={logo} alt='logo' /></li>
       <li className="mx-4 cursor-pointer hover:text-gray-700">Новости</li>
       <li className="mx-4 cursor-pointer hover:text-gray-700">Питомцы</li>
