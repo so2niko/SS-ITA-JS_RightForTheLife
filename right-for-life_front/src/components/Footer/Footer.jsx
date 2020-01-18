a70f82c83ff39830328f27760ef9c3b80ec74e74
alexboklag <sanya32102@gmail.com>
2020-01-18 20:11:59 +0200

    Add semantic tags, change import logo

--- right-for-life_front/src/components/Footer/Footer.jsx
+++ right-for-life_front/src/components/Footer/Footer.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 export const Footer = () => {
 	return (
-		<ul className="h-16 bg-lightgray-100 shadow-md flex justify-center items-center font-bold text-2xl text-gray-600">
-			<li className="ml-32">0932350370</li>
-			<li className="mx-6">
-				<img src={logo} alt="logo" />
-			</li>
-			<li>olyaum76@gmail.com</li>
-		</ul>
+		<footer>
+			<ul className="flex justify-center items-center py-2 bg-white shadow-md font-bold text-xl text-gray-600">
+				<li className="ml-24">0932350370</li>
+				<li className="mx-6">
+					<Logo />
+				</li>
+				<li>olyaum76@gmail.com</li>
+			</ul>
+		</footer>
 	);
 };
