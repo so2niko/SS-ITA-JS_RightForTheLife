d6a43b74cbbdf656d63355aafd8fb2763e4f7674
alexboklag <sanya32102@gmail.com>
2020-01-18 20:11:08 +0200

    Add semantic tags, change import logo

--- right-for-life_front/src/components/Header/Header.jsx
+++ right-for-life_front/src/components/Header/Header.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import logo from "../../assets/logo.svg";
+import { ReactComponent as Logo } from "../../assets/logo.svg";
 
 export const Header = () => {
 	return (
-		<ul className="fixed w-full h-16 bg-lightgray-100 shadow-md flex justify-center items-center font-bold text-2xl text-gray-600 z-50">
-			<li className="mx-4 cursor-pointer ">
-				<img src={logo} alt="logo" />
-			</li>
-			<li className="mx-4 cursor-pointer hover:text-gray-700">Новости</li>
-			<li className="mx-4 cursor-pointer hover:text-gray-700">Питомцы</li>
-			<li className="mx-4 cursor-pointer hover:text-gray-700">Товары</li>
-			<li className="mx-4 cursor-pointer hover:text-gray-700">
-				Счастливые истории
-			</li>
-		</ul>
+		<header>
+			<nav>
+				<ul className="fixed flex justify-center items-center py-2 w-full bg-white shadow-md font-bold text-xl text-gray-600 z-50">
+					<li className="mx-4 cursor-pointer ">
+						<Logo />
+					</li>
+					<li className="mx-4 cursor-pointer hover:text-gray-700">Новости</li>
+					<li className="mx-4 cursor-pointer hover:text-gray-700">Питомцы</li>
+					<li className="mx-4 cursor-pointer hover:text-gray-700">Товары</li>
+					<li className="mx-4 cursor-pointer hover:text-gray-700">
+						Счастливые истории
+					</li>
+				</ul>
+			</nav>
+		</header>
 	);
 };
