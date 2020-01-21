fcdd3161504ffd4e20bb4c40b7d9f165f70f6895
Aleksey Kasaev <aleksey.kasaev@gmail.com>
2020-01-21 11:44:56 +0200

    Configure store
    - add configureStore.js
    - add saga
    - add Provider to App.js

--- right-for-life_front/src/App.jsx
+++ right-for-life_front/src/App.jsx
@@ -1,15 +1,19 @@
 import React, { Fragment } from "react";
+import { Provider } from 'react-redux';
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { AnimalsPage } from "./containers/AnimalsPage";
 
-function App() {
+function App({store}) {
+
 	return (
-		<Fragment>
-			<Header />
-			<AnimalsPage />
-			<Footer />
-		</Fragment>
+    <Provider store={store}>
+			<Fragment>
+				<Header />
+				<AnimalsPage />
+				<Footer />
+			</Fragment>
+		</Provider>
 	);
 }
 
