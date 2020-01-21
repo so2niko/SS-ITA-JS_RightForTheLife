33d10b9cd18fca115dac5596c7ec064f08be447f
protonaby <protonaby@gmail.com>
2020-01-21 22:41:57 +0200

    02.02 Display list of news
    -add NewsPage to the appropriate route
    -limit news text to 120 symbols
    -tune styles for smaller screen

--- right-for-life_front/src/components/NewsItem/NewsItem.jsx
+++ right-for-life_front/src/components/NewsItem/NewsItem.jsx
@@ -4,16 +4,16 @@ export const NewsItem = (props) => {
   const {title, date, photo, text} = props.news;
 
   return (
-    <li className="md:w-2/5 sm:w-auto m-4 flex flex-col">
+    <li className="m-4 flex flex-col sm:w-2/5 w-3/4">
       <div className="pointer">
         <div className="h-64 rounded-xl bg-cover shadow-md bg-center"
              style={{backgroundImage: "url(" + photo + ")"}}></div>
       </div>
       <div className="z-40 w-19/20 bg-white text-lightgray-700 shadow-xl rounded-xl self-center -mt-10 px-5
-                      md:h-56 overflow-hidden flex flex-col justify-between sm:h-auto">
+                      lg:h-56 overflow-hidden flex flex-col justify-between h-auto">
         <p className="font-medium mt-2">{new Date(Number(date)).toLocaleDateString()}</p>
         <p className="uppercase mt-2 font-bold">{title}</p>
-        <p className="mt-2 font-size-sm">{text.split(/[.!]/)[0]}...</p>
+        <p className="mt-2 font-size-sm">{text.slice(0, 120)}...</p>
         <div className="text-right">
           <button className="min-w-5/12 bg-orange-300 hover:bg-orange-400 text-orange-700 font-bold py-2 px-2
                              rounded-xl mt-2 mb-4 ml-2" style={{outline: 'none'}}>
