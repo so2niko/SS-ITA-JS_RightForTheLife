877e3766229c8748e5b04d41b0d8269a4b817cf4
Vadim Topcheev <v.topcheev@gmail.com>
2020-01-23 10:28:03 +0200

    corrected with of the amount column

--- right-for-life_front/src/components/SuppliesTable/SuppliesTable.jsx
+++ right-for-life_front/src/components/SuppliesTable/SuppliesTable.jsx
@@ -47,7 +47,7 @@ export default class SuppliesTable extends Component {
           <tr className='text-2xl uppercase font-bold text-lightgray-700'>
             <th className='px-4 py-2'>Товар</th>
             <th className='px-4 py-2'>Тип</th>
-            <th className='px-4 py-2'>Кол-во</th>
+            <th className='px-4 py-2 w-32'>Кол-во</th>
             <th className='px-4 py-2'>Инфо</th>
           </tr>
         </thead>
