239e35cf199c1d3869776af31c9c83b64bc70d3f
yevhverb <51407533+yevhverb@users.noreply.github.com>
2020-01-23 11:49:50 +0200

    added page 404

--- right-for-life_front/src/App.jsx
+++ right-for-life_front/src/App.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 import { AnimalsPage } from "./containers/AnimalsPage";
 import { NewsPage } from "./containers/NewsPage";
-import {HappyStoriesPage} from "./containers/HappyStoriesPage";
+import { HappyStoriesPage } from "./containers/HappyStoriesPage";
+import { ErrorIndicator } from "./components/ErrorIndicator";
 
 function App({ store }) {
 	return (
 		<Provider store={store}>
 			<Router>
 				<Header />
-				<Switch>
-					<Route exact path="/" /*component={HomePage}*/ />
-					<Route exact path="/animals" component={AnimalsPage} />
-					<Route exact path="/news" component={NewsPage} />
-					<Route exact path="/stories" component={HappyStoriesPage} />
-					<Route exact path="/help" /*component={HelpPage}*/ />
-					<Route exact path="/about" /*component={AboutPage}*/ />
-					<Route exact path="/urgent" /*component={UrgentPage}*/ />
-				</Switch>
+				<article className="container mx-auto">
+					<Switch>
+						<Route exact path="/" /*component={HomePage}*/ />
+						<Route exact path="/animals" component={AnimalsPage} />
+						<Route exact path="/news" component={NewsPage} />
+						<Route exact path="/stories" component={HappyStoriesPage} />
+						<Route exact path="/help" /*component={HelpPage}*/ />
+						<Route exact path="/about" /*component={AboutPage}*/ />
+						<Route exact path="/urgent" /*component={UrgentPage}*/ />
+						<Route render={() => 
+							<ErrorIndicator 
+								message="Страница не найдена :("
+								renderAction={() => <Link to="/">Вернуться на главную</Link>}
+							/>} 
+						/>
+					</Switch>
+				</article>
 				<Footer />
 			</Router>
 		</Provider>
