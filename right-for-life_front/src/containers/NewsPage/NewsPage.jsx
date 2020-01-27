d8b2516002b33ca9d8f6ae926999a4cc87751b60
protonaby <protonaby@gmail.com>
2020-01-27 21:05:51 +0200

    06.03 Handle invalid news id

--- right-for-life_front/src/containers/NewsPage/NewsPage.jsx
+++ right-for-life_front/src/containers/NewsPage/NewsPage.jsx
@@ -1,12 +1,17 @@
 import React from "react";
-import {useParams} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import {withFetchDataIndicators} from "../../hoc/withFetchDataIndicators";
 import {Article} from "../../components/Article";
+import {ErrorIndicator} from "../../components/ErrorIndicator";
 
 const NewsPage = ({data}) => {
   let {id} = useParams();
   const article = data.find(article => article.id === Number(id));
-
+  if (!article)
+    return <ErrorIndicator
+      message="Страница не найдена :("
+      renderAction={() => <Link to="/">Вернуться на главную</Link>}
+    />;
   return (
     <div className="flex flex-wrap justify-center">
       <Article article={article}/>
