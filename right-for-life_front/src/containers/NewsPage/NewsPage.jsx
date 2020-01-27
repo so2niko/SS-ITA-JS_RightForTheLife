d7b0395946d740e7e1957786911ae1462f984965
protonaby <protonaby@gmail.com>
2020-01-27 09:18:07 +0200

    06.03 Create single news page markup and functionality
    - rename NewsPage container into NewsListPage
    - add NewsPage container for individual news article
    - add Article component for individual article markup
    - in ArticlesList component sort all incoming articles from newest to oldest

--- right-for-life_front/src/containers/NewsPage/NewsPage.jsx
+++ right-for-life_front/src/containers/NewsPage/NewsPage.jsx
@@ -1,14 +1,20 @@
 import React from "react";
-import {ArticlesList} from "../../components/ArticlesList";
-import { withFetchDataIndicators } from "../../hoc/withFetchDataIndicators";
+import {useParams} from "react-router-dom";
+import {withFetchDataIndicators} from "../../hoc/withFetchDataIndicators";
+import {Article} from "../../components/Article";
 
-const NewsPage = ({ data }) => {
+const NewsPage = ({data}) => {
+  let {id} = useParams();
+  const article = data.find(article => article.id === Number(id));
+
   return (
-      <ArticlesList articles={data} listTitle="Новости"/>
+    <div className="flex flex-wrap justify-center">
+      <Article article={article}/>
+    </div>
   );
 };
 
 const dataApi = 'https://raw.githubusercontent.com/protonaby/demo3-animal-shelter/master/db/news.json';
 const wrappedComponent = withFetchDataIndicators(NewsPage, dataApi);
 
-export { wrappedComponent as NewsPage };
+export {wrappedComponent as NewsPage};
