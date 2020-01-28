42e72511fa27c0d94fe1b0b971eed0bd62325cce
Oleksandr Boklah <sanya32102@gmail.com>
2020-01-28 13:29:43 +0200

    Some changes

--- right-for-life_front/src/components/ImageCarousel/ImageCarousel.jsx
+++ right-for-life_front/src/components/ImageCarousel/ImageCarousel.jsx
@@ -3,19 +3,16 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import { HOME } from '../../rootConstants';
-import { withFetchDataIndicators } from "../../hoc/withFetchDataIndicators";
+export const ImageCarousel = ({ data }) => {
+    const sliderSettings = {
+        className: "w-full h-80",
+        dots: true,
+        infinite: true,
+        speed: 2500,
+        autoplay: true,
+        autoplaySpeed: 5000
+    };
 
-const sliderSettings = {
-    className: "w-full h-80",
-    dots: true,
-    infinite: true,
-    speed: 2500,
-    autoplay: true,
-    autoplaySpeed: 5000
-};
-
-const ImageCarousel = ({data}) => {
     return (
         <div className="h-80 flex items-center justify-center rounded-xl mb-4">
             <Slider {...sliderSettings}>
@@ -31,8 +28,3 @@ const ImageCarousel = ({data}) => {
         </div>
     )
 };
-
-const dataApi = 'https://raw.githubusercontent.com/AlexeyKasaev3/softServe-academy/master/demo-3-data/news.json';
-const wrappedComponent = withFetchDataIndicators(ImageCarousel, HOME, dataApi);
-
-export { wrappedComponent as ImageCarousel };
