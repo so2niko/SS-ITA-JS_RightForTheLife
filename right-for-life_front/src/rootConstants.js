dd520a100c68d1b7f5f7b0b814af175abca9db25
Oleksandr Boklah <sanya32102@gmail.com>
2020-01-28 12:03:54 +0200

    Add DONATE state

--- right-for-life_front/src/rootConstants.js
+++ right-for-life_front/src/rootConstants.js
@@ -2,6 +2,7 @@ export const ANIMALS = 'ANIMALS';
 export const NEWS = 'NEWS';
 export const HAPPY_STORIES = 'HAPPY_STORIES';
 export const HELP = 'HELP';
+export const DONATE = 'DONATE';
 export const SUPPLIES = 'SUPPLIES';
 export const ABOUT_US = 'ABOUT_US';
 export const EMERGENCY_HELP = 'EMERGENCY_HELP';
