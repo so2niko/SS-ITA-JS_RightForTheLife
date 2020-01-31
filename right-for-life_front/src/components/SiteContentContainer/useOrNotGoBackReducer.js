52c5669c90bc5897f8ccc2e1f46123b7631ef6f4
Maxim Voloshin <maksv21@gmail.com>
2020-01-31 18:28:09 +0200

    rename

--- right-for-life_front/src/components/SiteContentContainer/useOrNotGoBackReducer.js
+++ right-for-life_front/src/components/SiteContentContainer/useOrNotGoBackReducer.js
@@ -1,6 +1,6 @@
 import { UPD_BACK_BTN_STATUS } from '../../rootConstants';
 
-export const setUseOrNotGoBack = (state = false, action) => {
+export const useOrNotGoBackReducer = (state = false, action) => {
   const {type, payload} = action;
 
   if (type === UPD_BACK_BTN_STATUS) {
