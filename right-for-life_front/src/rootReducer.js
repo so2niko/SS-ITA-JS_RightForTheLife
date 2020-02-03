52cec28c20b662e63750e020369e8eba00c46a65
yevhverb <51407533+yevhverb@users.noreply.github.com>
2020-02-03 20:38:56 +0200

    some fixes

--- right-for-life_front/src/rootReducer.js
+++ right-for-life_front/src/rootReducer.js
@@ -3,5 +3,6 @@ import { fetchDataReducer } from './hoc/withFetchDataIndicators';
 import { urlForBackBtnReducer as urlForBackBtn } from './components/SiteContentContainer/';
 
 export const rootReducer = combineReducers({
-  fetchDataReducer, urlForBackBtn,
+  fetchDataReducer, 
+  urlForBackBtn,
 });
