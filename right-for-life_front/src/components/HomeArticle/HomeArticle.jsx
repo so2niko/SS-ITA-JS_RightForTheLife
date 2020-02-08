f1368ae2a55f837f278abc4d744925af52ea29aa
Oleksandr Boklah <sanya32102@gmail.com>
2020-02-08 16:19:31 +0200

    Added HomeArticle component

---
+++ right-for-life_front/src/components/HomeArticle/HomeArticle.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const HomeArticle = ({ data: { textHome } }) => {
+    const title = "Фонд \"Мы за право на жизнь\"";
+
+    return (
+        <article className="p-5">
+            <h1 className="mb-3 text-3xl font-bold text-lightgray-700">
+                {title}
+            </h1>
+            {
+                textHome.map((item, index) => (
+                    <p key={index} className="mt-2 text-justify text-lightgray-800">
+                        {item}
+                    </p>
+                ))
+            }
+
+        </article>
+    );
+}
