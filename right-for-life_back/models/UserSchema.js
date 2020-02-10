42781fa0c5cc550fcba1971e4637d37bfe0a368f
Oleksandr Boklah <sanya32102@gmail.com>
2020-02-10 23:04:27 +0200

    Update email match

--- right-for-life_back/models/UserSchema.js
+++ right-for-life_back/models/UserSchema.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const AnimalSchema = new mongoose.Schema(
   {
     _id: mongoose.Schema.Types.ObjectId,
-    email: { type: String, required: true, match: /[a-zA-Z0-9_\-.]*@[a-zA-Z]*/g },
-    password: { type: String, required: true, match: /.{8}/g },
+    email: { type: String, required: true, match: /[a-zA-Z0-9_\-.]+@[a-zA-Z]{1}[a-zA-Z.]+/g },
+    password: { type: String, required: true, match: /.{8,}/g },
     username: { type: String, required: true },
   });
 
