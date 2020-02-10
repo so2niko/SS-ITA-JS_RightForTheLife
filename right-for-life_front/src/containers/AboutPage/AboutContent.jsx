ca351d23b6a58260df26756894f75c38c0b9bc27
Maxim Voloshin <maksv21@gmail.com>
2020-02-10 23:52:44 +0200

    added key to paragraph

--- right-for-life_front/src/containers/AboutPage/AboutContent.jsx
+++ right-for-life_front/src/containers/AboutPage/AboutContent.jsx
@@ -6,8 +6,8 @@ const AboutContent = ({photos, text}) => (
   <div className='text-justify'>
     <Gallery photos={photos} alt="О нас"/>
 
-    {text.map(paragraph => (
-      <p className="my-3 rounded-xl bg-gray-300 text-gray-700 p-5 font-medium">
+    {text.map((paragraph, index) => (
+      <p className="my-3 rounded-xl bg-gray-300 text-gray-700 p-5 font-medium" key={index}>
         {paragraph}
       </p>
     ))}
