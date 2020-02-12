3c14c5ad9e2ed077ebaaf9e1df29c5c84022329c
Aleksey Kasaev <aleksey.kasaev@gmail.com>
2020-02-12 07:34:47 +0200

    create article video and featured image edit components

--- right-for-life_front/src/components/Article/Article.jsx
+++ right-for-life_front/src/components/Article/Article.jsx
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import calcAge from '../../helpers/calcAge';
 import { BackBtn, ShareBtn } from '../FloatingBtn';
 import YouTube from 'react-youtube';
 import {ArticleImageGallery} from "../ArticleImageGallery";
 import { DonateButton } from "../DonateButton";
 import { useLocation } from 'react-router-dom';
-import { extractVideoIdFromYouTubeLink } from "../../helpers/extractVideoIdFromYouTubeLink";
-import { EditArticleFeaturedImage } from "../EditArticleFeaturedImage";
+
 
 import './style.css';
 
 export const Article = ({article}) => {
-  const [originalArticleData, setOriginalArticleData] = useState(article);
-  const [modifiedArticleData, setModifiedArticleData] = useState(article);
-  const isEditMode = true;
-
   const {title, date, photo, text, gallery, videos} = article;
   const { pathname: currentURL } = useLocation();
 
@@ -23,18 +18,14 @@ export const Article = ({article}) => {
       <BackBtn position="left-0 ml-2 mt-6"/>
       <ShareBtn position="right-0 mr-2 mt-6"/>
 
-        {isEditMode ? (
-          <EditArticleFeaturedImage image={article.photo} imageChangeHandler={(value => console.log(value))}/>
-          ) : (
-          <div
-            className="h-78 rounded-b-xl bg-cover shadow-md bg-center flex"
-            style={{backgroundImage: "url(" + (isEditMode ? modifiedArticleData.photo : originalArticleData.photo)  + ")"}}
-          />
-        )
-          }
+      <div
+        className="h-78 rounded-b-xl bg-cover shadow-md bg-center"
+        style={{backgroundImage: "url(" + photo + ")"}}
+      >
+      </div>
       <div className="uppercase font-extrabold text-xl z-20 relative bg-white text-lightgray-700
                  shadow-xl rounded-xl -mt-10 px-8 flex items-center mx-8 mb-10 justify-between"
-          style={{minHeight: "100px"}}
+           style={{minHeight: "100px"}}
       >
         {title}
         {currentURL.includes('emergency') ? <DonateButton style={{marginLeft:'10px'}}/> : null}
@@ -46,18 +37,18 @@ export const Article = ({article}) => {
           {calcAge(Number(date))} назад
         </aside>
       </div>
-        { gallery?.length ? (
-          <div className="mb-10 md:mx-20">
-            <ArticleImageGallery images={gallery} />
-          </div>
-        ) : null }
+      { gallery?.length ? (
+        <div className="mb-10 md:mx-20">
+          <ArticleImageGallery images={gallery} />
+        </div>
+      ) : null }
       <div className="mx-10 md:mx-20">
         <p className="font-medium text-lightgray-600 mb-12">
           {text}
         </p>
         { videos?.length ? videos.map(video =>
           <div className="video-iframe-container" key={video}>
-            <YouTube videoId={extractVideoIdFromYouTubeLink(video)} />
+            <YouTube videoId={video} />
           </div>) : null }
       </div>
     </article>
