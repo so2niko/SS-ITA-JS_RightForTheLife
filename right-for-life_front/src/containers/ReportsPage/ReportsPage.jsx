9224ae99be2252abe73130bbb85da1b812c90506
Aleksey Kasaev <aleksey.kasaev@gmail.com>
2020-02-12 10:27:10 +0200

    test google drive embeded code

--- right-for-life_front/src/containers/ReportsPage/ReportsPage.jsx
+++ right-for-life_front/src/containers/ReportsPage/ReportsPage.jsx
@@ -9,6 +9,7 @@ const ReportsPage = ({data}) => {
   return (
     <div>
       <ArticlesList articles={data} listTitle="Финансовые отчеты"/>
+      <iframe src="https://drive.google.com/embeddedfolderview?id=1HMPNd9HcedJLD4j40TS3bxmNOAHx5-4a#grid" style={{width: '100%', height: '600px', border: '1px solid red'}} />
     </div>
   );
 };
