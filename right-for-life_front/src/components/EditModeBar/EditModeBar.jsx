06e16584aa713af97b6c5ddced051aca2361fb91
yevhverb <51407533+yevhverb@users.noreply.github.com>
2020-02-13 20:59:41 +0200

    update edit mode bar

--- right-for-life_front/src/components/EditModeBar/EditModeBar.jsx
+++ right-for-life_front/src/components/EditModeBar/EditModeBar.jsx
@@ -2,11 +2,12 @@ import React, { useState } from 'react';
 
 export const EditModeBar = ({ state, onEdit }) => {
   const [open, setIsOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(true); 
 
   return (
     <>
       <button 
-        className={`fixed right-0 bottom-0 flex items-center justify-center h-14 w-14 m-10 rounded-full font-bold text-green-800 bg-green-200 ${!open ? 'visible' : 'invisible'}`}
+        className={`fixed right-0 bottom-0 flex items-center justify-center h-14 w-14 m-12 rounded-full font-bold shadow-xl text-lightgray-700 bg-white ${!open ? 'visible' : 'invisible'}`}
         onClick={() => {
           setIsOpen(true);
           onEdit();
@@ -16,16 +17,17 @@ export const EditModeBar = ({ state, onEdit }) => {
       <article className={`fixed z-50 inset-x-0 bottom-0 flex justify-center py-2 text-sm shadow-xl bg-green-200 ${open ? 'visible' : 'invisible'}`}>
         <section>
           <button 
-            className="mx-2 px-4 py-2 rounded-lg font-bold text-green-800 bg-green-300 uppercase"
+            className="mx-2 px-4 py-2 rounded-lg font-bold text-green-800 bg-green-300 hover:bg-green-400 uppercase"
             onClick={() => {
+              setIsEdit(!isEdit);
               onEdit();
             }}>
-            Предпросмотр
+            {isEdit ? 'Предпросмотр' : 'Редактировать'}
           </button>
         </section>
         <section>
           <button 
-            className="mx-2 px-4 py-2 rounded-lg font-bold text-green-800 bg-green-300 uppercase"
+            className="mx-2 px-4 py-2 rounded-lg font-bold text-green-800 bg-green-300 hover:bg-green-400 uppercase"
             onClick={() => {
               setIsOpen(false);
               onEdit();
@@ -34,7 +36,7 @@ export const EditModeBar = ({ state, onEdit }) => {
             Сохранить
           </button>
           <button 
-            className="mx-2 px-4 py-2 rounded-lg font-bold text-green-800 bg-green-300 uppercase"
+            className="mx-2 px-4 py-2 rounded-lg font-bold text-green-800 bg-green-300 hover:bg-green-400 uppercase"
             onClick={() => {
               window.location.reload();
             }}>
