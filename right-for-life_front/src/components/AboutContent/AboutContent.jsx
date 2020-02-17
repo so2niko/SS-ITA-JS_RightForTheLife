ed14c3424da3d71c61dd585837cbf37a3409cb91
Vadym Topchieiev <v.topcheev@gmail.com>
2020-02-17 13:38:09 +0200

    images and text edit
    addded images and text edit

--- right-for-life_front/src/components/AboutContent/AboutContent.jsx
+++ right-for-life_front/src/components/AboutContent/AboutContent.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-const AboutContent = ({ text }) => {
-  if (!text) {
-    return null;
-  }
+const AboutContent = ({ text, isEditable, setNewText }) => {
   return (
     <div>
       <div className="text-justify md:mx-16 mx-4">
         {text.map((par, i) => {
           return (
-            <p className="my-3 rounded-xl bg-gray-300 p-5" key={i}>
+            <p
+              className="my-3 rounded-xl bg-gray-300 p-5"
+              style={{ whiteSpace: 'pre-wrap' }}
+              contentEditable={isEditable}
+              suppressContentEditableWarning
+              key={i}
+              onBlur={e => {
+                setNewText(i, e.target.innerHTML);
+              }}
+            >
               {par}
             </p>
           );
--- right-for-life_front/src/containers/AboutPage/AboutPage.jsx
+++ right-for-life_front/src/containers/AboutPage/AboutPage.jsx
@@ -1,21 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { API } from '../../rootConstants';
 import { withFetchDataIndicators } from '../../hoc/withFetchDataIndicators';
 import AboutContent from '../../components/AboutContent';
 import AboutContacts from '../../components/AboutContacts';
 import { ImageCarousel } from '../../components/ImageCarousel';
+import { UpdateImageGallery } from '../../components/UpdateImageGallery';
 
 const AboutPage = ({ data }) => {
-  const { gallery, description, facebook, instagram, phone, email } = data;
+  const [state, setState] = useState(data);
+  const [isEdit, setIsEdit] = useState(true);
+  const { gallery, description, facebook, instagram, phone, email } = state;
+  const setNewDescription = (i, text) => {
+    // console.log(i, text);
+    // console.log(state.description[i]);
+    const textArr = [...state.description];
+    textArr[i] = text;
+    setState({ ...state, description: textArr });
+    // setState({ ...state, description: text });
+  };
 
   return (
     <div className="about-page pb-8 px-5">
-      <h2 className="mb-6 px-4 font-bold text-lightgray-700 text-4xl uppercase">О нас</h2>
+      <h2 className="mb-6 px-4 font-bold text-lightgray-700 text-4xl uppercase">
+        О нас
+      </h2>
       <ImageCarousel data={gallery} page="about" />
-      <AboutContent text={description} />
-      <AboutContacts
-        contactsData={{ facebook, phone, email, instagram }}
+      <UpdateImageGallery
+        images={gallery}
+        updateImages={gallery => setState({ ...state, gallery })}
       />
+      <AboutContent
+        text={description}
+        isEditable={isEdit}
+        setNewText={setNewDescription}
+      />
+      <AboutContacts contactsData={{ facebook, phone, email, instagram }} />
     </div>
   );
 };
