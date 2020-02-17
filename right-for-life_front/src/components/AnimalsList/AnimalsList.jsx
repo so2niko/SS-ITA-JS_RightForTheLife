994efaee815aabf5b660a6d1658772b3ca9dd14e
Oleksandr Boklah <sanya32102@gmail.com>
2020-02-17 09:16:12 +0200

    Fix bugs

--- right-for-life_front/src/components/AnimalsList/AnimalsList.jsx
+++ right-for-life_front/src/components/AnimalsList/AnimalsList.jsx
@@ -4,18 +4,18 @@ import { withPagination } from '../../hoc/withPagination';
 import { withFetchDataIndicators } from '../../hoc/withFetchDataIndicators';
 import { AnimalCard } from '../AnimalCard/AnimalCard';
 
-const AnimalsList = ({ data }) => {
-  console.log(data);
-return (
-  <ul className="flex flex-wrap justify-center mb-8">
-    {data.map(animal => (
-      <AnimalCard key={animal._id} animal={animal} />
-    ))}
-  </ul>
-)};
+const AnimalsList = ({ data: { docs: animals } }) => {
+  return (
+    <ul className="flex flex-wrap justify-center mb-8">
+      {animals.map(animal => (
+        <AnimalCard key={animal._id} animal={animal} />
+      ))}
+    </ul>
+  )
+};
 
 const wrappedComponent = withFetchDataIndicators(
-  withPagination(AnimalsList, 8),
+  AnimalsList,
   API.ANIMALS,
   true,
 );
