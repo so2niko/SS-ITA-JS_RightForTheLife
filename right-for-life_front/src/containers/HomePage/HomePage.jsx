e9856a2ebebf7ff68c162511e82aa25522c12f70
yevhverb <51407533+yevhverb@users.noreply.github.com>
2020-02-18 19:57:22 +0200

    update the home page for edit data

--- right-for-life_front/src/containers/HomePage/HomePage.jsx
+++ right-for-life_front/src/containers/HomePage/HomePage.jsx
@@ -1,21 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { API } from '../../rootConstants';
+import { CUDService } from '../../services/CUDService';
+import { withFetchDataIndicators } from '../../hoc/withFetchDataIndicators';
 import { ImageCarousel } from '../../components/ImageCarousel';
 import { HomeArticle } from '../../components/HomeArticle';
 import { ArticlesListWidget } from '../../components/ArticlesListWidget';
-import { withFetchDataIndicators } from '../../hoc/withFetchDataIndicators';
+import { EditModeBar } from '../../components/EditModeBar';
+import { Select } from '../../components/Select';
+import { UpdateImageGallery } from '../../components/UpdateImageGallery';
 
-const HomePage = ({ data }) => (
-  <article className="flex flex-col lg:flex-row flex-1 pb-8">
-    <section className="w-full lg:w-2/3 p-5">
-      <ImageCarousel data={data.gallery} page="home" />
-      <HomeArticle {...data} />
-    </section>
-    <section className="w-full lg:w-1/3 lg:h-auto p-5">
-      <ArticlesListWidget url="news" />
-    </section>
-  </article>
-);
+const HomePage = ({ data }) => {
+  const [state, setState] = useState(data);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isEditModeBarOpen, setIsEditModeBarOpen] = useState(false);
+
+  const selectOptionChoseHandler = selectedOption => {
+    switch (selectedOption) {
+      case 'edit':
+        setIsEditModeBarOpen(true);
+        setIsEdit(true);
+        break;
+      case 'no-edit':
+        setIsEditModeBarOpen(false);
+        setIsEdit(false);
+        break;
+      default:
+        return null;
+    }
+  };
+
+  const setTitle = title => setState({ ...state, title });
+  const setDescription = description => setState({ ...state, description });
+
+  return (
+    <article className="flex flex-col lg:flex-row flex-1 pb-8">
+      {isEditModeBarOpen ? (
+        <EditModeBar
+          data={state}
+          onEdit={() => setIsEdit(!isEdit)}
+          onSave={() => {
+            CUDService.PUT('/home', state);
+            selectOptionChoseHandler('no-edit');
+          }}
+          onCancel={() => {
+            selectOptionChoseHandler('no-edit');
+          }}
+        />
+      ) : (
+        <Select
+          classNames="fixed z-50 top-0 right-0 mr-10 mt-20"
+          chooseOptionHandler={selectOptionChoseHandler}
+          optEdit
+        />
+      )}
+      <section className="w-full lg:w-2/3 p-5">
+        {!isEdit ? (
+          <ImageCarousel data={state.gallery} />
+        ) : (
+          <UpdateImageGallery
+            images={state.gallery}
+            updateImages={gallery => setState({ ...state, gallery })}
+          />
+        )}
+        <HomeArticle
+          title={state.title}
+          description={state.description}
+          setTitle={setTitle}
+          setDescription={setDescription}
+          isEdit={isEdit}
+        />
+      </section>
+      <section className="w-full lg:w-1/3 lg:h-auto p-5">
+        <ArticlesListWidget url="news" />
+      </section>
+    </article>
+  );
+};
 
 const wrappedComponent = withFetchDataIndicators(HomePage, API.HOME);
 
