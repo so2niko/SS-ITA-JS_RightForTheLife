b722bbced4f6de1d13254f00d76431566aa98c72
Oleksandr Boklah <sanya32102@gmail.com>
2020-02-20 09:58:44 +0200

    Some fixes

--- right-for-life_front/src/components/TabBar/TabBar.jsx
+++ right-for-life_front/src/components/TabBar/TabBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Tab from '../Tab';
 import DonateInfo from '../DonateInfo';
 import SuppliesTable from '../SuppliesTable';
