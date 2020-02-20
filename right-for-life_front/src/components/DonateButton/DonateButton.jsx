2f7d97a192cee8df1b6755b39125e661db432d4b
Ivan Bashmak <kiit15.bashmak@gmail.com>
2020-02-20 14:19:13 +0200

    add token to button

--- right-for-life_front/src/components/DonateButton/DonateButton.jsx
+++ right-for-life_front/src/components/DonateButton/DonateButton.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
+import { API } from '../../rootConstants';
+import { withFetchDataIndicators } from '../../hoc/withFetchDataIndicators';
 
-export const DonateButton = ({ href, className, style }) => {
+const DonateButton = ({ className, style, data }) => {
+  console.log('PRIVAT TOKEN', data.privat24Token)
   return (
     <a
       className={className}
       rel="noopener noreferrer"
       target="_blank"
-      href={href}
+      href={`https://next.privat24.ua/payments/form/%7B%22token%22:%22${data.privat24Token}%22%7D`}
       style={style}
     >
       Помочь
     </a>
   );
 };
-/*
-https://next.privat24.ua/payments/form/{"token":"cc67a469-2b4a-419a-8850-f52c596fd1b0"}
-*/
+
 DonateButton.defaultProps = {
   style: null,
-  href: 'https://next.privat24.ua/payments',
   className:
     'min-w-5/12 bg-yellow-300 text-yellow-700 hover:bg-yellow-400 hover:text-yellow-800 font-bold py-1 px-3 rounded-xl',
 };
+
+const wrappedComponent = withFetchDataIndicators(DonateButton, API.DONATE);
+
+export { wrappedComponent as DonateButton };
